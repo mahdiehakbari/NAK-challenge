ea3c9b5b3155f33407d78d28e82a99c9e2bd9c95
mahdieh <mahdiehakbari16@gmail.com>
2025-08-04 19:37:09 +0330

    feat(routes): add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css';
-import { SignInForm } from './features/auth/components/SignInForm';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { SignInForm } from './features/auth/components/SignIn/SignInForm';
+import SignUpPage from './features/auth/components/Signup';
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>ورود به سامانه</h1>
-        <SignInForm />
-      </div>
-    </>
+    <Routes>
+      {/* ریدایرکت پیش‌فرض به لاگین */}
+      <Route path='/' element={<Navigate to='/login' replace />} />
+
+      <Route path='/login' element={<SignInForm />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      {/* <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/attribute' element={<Attribute />} />
+
+        {/* صفحه 404 ساده */}
+      {/* <Route path='*' element={<div>Page not found</div>} />  */}
+    </Routes>
   );
 }
 
