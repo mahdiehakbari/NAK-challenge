4c6dbf7b077470434b95a1a7babe7aff37701a42
mahdieh <mahdiehakbari16@gmail.com>
2025-08-04 19:52:34 +0330

    feat(routes): add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import './i18n.ts';
+import App from './App';
+import './i18n';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
